fcf97a394df152acb8d13f963cb6d7b5effc114f
allllfo <mirae4292@gmail.com>
2024-01-31 08:14:58 +0900

    delete : .DS_Store삭제

--- stock_crawl/ex_stock_crawl.js
+++ stock_crawl/ex_stock_crawl.js
@@ -21,8 +21,14 @@ async function fetchNaverStock(code){
     // console.log($('.type 2'));
     // console.log(sise);
 
-    $('tr').slice(1,).map((i,e)=> {
-        tdTag = $(el).find('td');
+    $('tr').slice(1,trTag.length-1).map((i,e)=> {
+        const date = $(el).find('td:nth-child(1)')?.text()?.trim(); //css에서 몇번쨰 자식인지에 따라서, 적용할 수 있는 것이 다름. (2n)이라고 하면 두번째마다 속성 적용 가능/ 2n+1 하면 홀수행마다 적용
+        const close = $(el).find('td:nth-child(2)')?.text()?.trim();
+        const ratio = $(el).find('td:nth-child(3)')?.text()?.trim();
+        const open = $(el).find('td:nth-child(4)')?.text()?.trim();
+        const high = $(el).find('td:nth-child(5)')?.text()?.trim();
+        const low = $(el).find('td:nth-child(6)')?.text()?.trim();
+        const volume = $(el).find('td:nth-child(6)')?.text()?.trim();
         if(tdTag.length <7){
             return {
             date : '',
@@ -38,11 +44,17 @@ async function fetchNaverStock(code){
             date : tdTag[0].text(),
             close : tdTag[1].text(),
             ratio: tdTag[2].text(),
-            open:tdTag[3].text(),
+            open: tdTag[3].text(),
             high:tdTag[4].text(),
             low:tdTag[5].text(),
             volum:tdTag[6].text(),
         }
         
     }).get();
+
+    const result = data.filter((elem, idx)=>{ //완성 안됨
+        if(elem.date){
+            return true
+        }
+    })
 }
